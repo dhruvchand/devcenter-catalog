03f965a04f599637faf704d8c10f34318a1dbab8
Dhruv Chand <dhruvchand@live.com>
2023-11-14 11:18:24 -0800

    ado-artifact task

--- Tasks/setup-design-system/setup.ps1
+++
@@ -1,6 +0,0 @@
-$rootDirectory="C:/Projects"
-New-Item $rootDirectory -ItemType Directory -Force
-Set-Location $rootDirectory
-git clone https://github.com/IRCC-ca/ds-sdc-dev.git
-Set-Location 'ds-sdc-dev' 
-npm install
